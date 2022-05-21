af6bb6fe9a7957ebb620ec1b2b17f9adc5ce2d51
leo-valadao <leonardovaladaooliveira@hotmail.com>
2022-05-21 18:29:18 -0300

    Atualização dos arquivos do banco de dados.

--- back_end/database.js
+++ back_end/database.js
@@ -1,11 +1,11 @@
 const {Client} = require('pg')
 
-const cliente = new Client({
-    host: "localhost",
-    user: "postgres",
-    port: 5432,
-    password: "admin",
-    database: "postgres"
+const cliente = new Client({ 
+    host: "localhost", // Endereço do Host
+    user: "postgres", // Usuário do PostgresSQL
+    password: "admin", // Senha do usuário do PostgresSQL
+    port: 5432, // Porta do PostgresSQL
+    database: "postgres" // Database do App no PostegreSQL
 })
 
 module.exports = cliente
